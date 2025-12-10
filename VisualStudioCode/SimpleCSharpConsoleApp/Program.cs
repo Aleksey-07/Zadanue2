d12adc3fd4187ee5b1691ada6801ceda5a4e0cfb
Aleksey <alex.nenakhov@yandex.ru>
2025-12-10 13:20:26 +0300

    first commit

---
+++ VisualStudioCode/SimpleCSharpConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var numbers = new[] { 1, 2, 3, 4, 5 };
+        var sum = numbers.Sum();
+        Console.WriteLine("Сумма: " + sum);
+    }
+}
